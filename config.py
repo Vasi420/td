ef3f3a1b5a0c1fe9ee71f06e676678de9aff8f20
Vasi <157385207+Vasi420@users.noreply.github.com>
2024-06-18 13:00:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,3 +10,19 @@ MONGO_DB_URL = getenv(
     "MONGO_DB_URL",
     "mongodb+srv://bsdk:betichod@cluster0.fgj1r9z.mongodb.net/?retryWrites=true&w=majority",
 )
+from os import getenv
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_ID = "10046731"
+# -------------------------------------------------------------
+API_HASH = "09a25ea3e558df7e5d969c144b0452d9"
+# --------------------------------------------------------------
+BOT_TOKEN = getenv("BOT_TOKEN", None)
+MONGO_URL = getenv("MONGO_URL", None)
+OWNER_ID = getenv("OWNER_ID", "5981008313")
+SUPPORT_GRP = "the_comedy_bar"
+UPDATE_CHNL = "AS_Bots_Updates"
+OWNER_USERNAME = "Oxygen_420"
